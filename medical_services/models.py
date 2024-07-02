d30308b103372b15044f096931bf2b5878a2e144
gremvanek <gremvanek@gmail.com>
2024-07-02 13:15:28 +0500

    Создание модели Мед.услуг. Создание и применение миграций.

--- medical_services/models.py
+++ medical_services/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db import models
 
 NULLABLE = {'null': True, 'blank': True}
@@ -16,3 +15,21 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Service(models.Model):
+    """ Медицинская услуга """
+    name = models.CharField(max_length=100, verbose_name='Название мед.услуги')
+    description = models.TextField(verbose_name='Описание мед.услуги')
+    image = models.ImageField(upload_to='services/', verbose_name='Превью мед.услуги', **NULLABLE)
+    price = models.PositiveIntegerField(verbose_name='Цена мед.услуги')
+    # Используется Положительное число. Цена не может быть отрицательной.
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    deadline = models.CharField(max_length=100, verbose_name='Срок выполнения мед.услуги')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Мед.услуга"
+        verbose_name_plural = "Мед.услуги"
