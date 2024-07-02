13c76acc954a35e2facd0674f9867581174e657e
gremvanek <gremvanek@gmail.com>
2024-07-02 14:36:58 +0500

    Добавил форму для Услуг.

--- medical_services/forms.py
+++ medical_services/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from medical_services.models import Category
+from medical_services.models import Category, Service
 
 
 class CategoryForm(forms.ModelForm):
@@ -16,3 +16,14 @@ class CategoryForm(forms.ModelForm):
             field.widget.attrs['class'] = 'form-control'
 
 
+class ServicesForm(forms.ModelForm):
+    """Форма для услуг"""
+
+    class Meta:
+        model = Service
+        fields = ('name', 'description', 'image', 'category', 'price', 'deadline')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
