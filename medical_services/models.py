520fff5020b2bcc87c09d4ed4e05cab8af21357f
gremvanek <gremvanek@gmail.com>
2024-07-02 13:08:38 +0500

    Добавил категорию медицинских услуг.

--- medical_services/models.py
+++ medical_services/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    """ Категории услуг """
+    name = models.CharField(max_length=100, verbose_name='Название категории')
+    description = models.TextField(verbose_name='Описание услуги')
+    image = models.ImageField(upload_to='categories/', verbose_name='Изображение', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
